355a80fe1fa06396ea38d93449460900f2645b67
yuyi5187 <kimyy200063@gmail.com>
2025-05-06 15:44:16 +0900

    First Commit

---
+++ src/test/java/likelion13th/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion13th.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
